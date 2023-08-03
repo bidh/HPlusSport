34eda78a2b6ab5d2709529f3e16160e9449ca5fc
bidh <birajdh@outlook.com>
2023-08-03 16:07:18 +0200

    Adding versioning in the API

--- HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -5,13 +5,16 @@ using System.Globalization;
 
 namespace HPlusSport.API.Controllers
 {
-    [Route("api/[controller]")]
+    [ApiVersion("1.0")]
+    //[Route("v{v:apiVersion}/products")]
+    //[Route("api/[controller]")]
+    [Route("products")]
     [ApiController]
-    public class ProductsController : ControllerBase
+    public class Products1Controller : ControllerBase
     {
         private readonly ShopContext _context;
 
-        public ProductsController(ShopContext context)
+        public Products1Controller(ShopContext context)
         {
             _context = context;
 
@@ -142,6 +145,167 @@ namespace HPlusSport.API.Controllers
         [HttpPost]
         [Route("Delete")]
         public async Task<ActionResult> DeleteMultiple([FromQuery]int[] ids)
+        {
+            var products = new List<Product>();
+            foreach (var id in ids)
+            {
+                var product = await _context.Products.FindAsync(id);
+
+                if (product == null)
+                {
+                    return NotFound();
+                }
+
+                products.Add(product);
+            }
+
+            _context.Products.RemoveRange(products);
+            await _context.SaveChangesAsync();
+
+            return Ok(products);
+        }
+    }
+
+    [ApiVersion("2.0")]
+    //[Route("v{v:apiVersion}/products")]
+    //[Route("api/[controller]")]
+    [Route("products")]
+    [ApiController]
+    public class Products2Controller : ControllerBase
+    {
+        private readonly ShopContext _context;
+
+        public Products2Controller(ShopContext context)
+        {
+            _context = context;
+
+            _context.Database.EnsureCreated();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllProducts([FromQuery] ProductQueryParameters queryParameters)
+        {
+            IQueryable<Product> products = _context.Products.Where(p=>p.IsAvailable==true);
+
+            if (queryParameters.MinPrice != null)
+            {
+                products = products.Where(x => x.Price >= queryParameters.MinPrice);
+            }
+
+            if (queryParameters.MaxPrice != null)
+            {
+                products = products.Where(x => x.Price <= queryParameters.MaxPrice);
+            }
+
+            if (!string.IsNullOrEmpty(queryParameters.SearchTerm))
+            {
+                products = products.Where(
+                    x => x.Sku.ToLower().Contains(queryParameters.SearchTerm.ToLower()) ||
+                         x.Name.ToLower().Contains(queryParameters.SearchTerm.ToLower()));
+            }
+
+            if (!string.IsNullOrEmpty(queryParameters.Sku))
+            {
+                products = products.Where(x => x.Sku == queryParameters.Sku);
+            }
+
+            if (!string.IsNullOrEmpty(queryParameters.Name))
+            {
+                products = products.Where(
+                    p => p.Name.ToLower().Contains(
+                        queryParameters.Name.ToLower()));
+            }
+
+            if (!string.IsNullOrEmpty(queryParameters.SortBy))
+            {
+                if (typeof(Product).GetProperty(queryParameters.SortBy) != null)
+                {
+                    products = products.OrderByCustom(queryParameters.SortBy, queryParameters.SortOrder);
+                }
+            }
+
+            products = products
+                .Skip(queryParameters.Size * (queryParameters.Page - 1))
+                .Take(queryParameters.Size);
+
+            return Ok(await products.ToArrayAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product product)
+        {
+            /*
+            if (!ModelState.IsValid) {
+                return BadRequest();
+            }
+            */
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                "GetProduct",
+                new { id = product.Id },
+                product);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Products.Any(p => p.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Product>> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+
+        [HttpPost]
+        [Route("Delete")]
+        public async Task<ActionResult> DeleteMultiple([FromQuery] int[] ids)
         {
             var products = new List<Product>();
             foreach (var id in ids)
